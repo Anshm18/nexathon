ab8eedb41993e99be43f93beedb996e7d252f56e
Ansh <anshmalhotra9871432012@gmail.com>
2025-04-09 02:17:45 +0530

    'with_backend'

---
+++ Frontend/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDLZE3lz8K9HvRpWVp7zR0YApua2NOFbU",
+  authDomain: "pet-adoption-21743.firebaseapp.com",
+  projectId: "pet-adoption-21743",
+  storageBucket: "pet-adoption-21743.firebasestorage.app",
+  messagingSenderId: "579646871885",
+  appId: "1:579646871885:web:58cab2a0eb30f76580ff04"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
